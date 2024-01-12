5ed0c7f432a5d3abb03fd8b2d5713450792e8df3
Harish_9795 <jkaalees9597@gmail.com>
2024-01-12 11:08:18 +0530

    added the design file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+export const routes: Routes = [
+    {
+        path:'login',
+        component:LoginComponent
+    },
 
-export const routes: Routes = [];
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+];
+
+
