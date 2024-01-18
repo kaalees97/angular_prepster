f246ea635d9812719ff9aad81dc44cd2719a45b6
Harish_9795 <jkaalees9597@gmail.com>
2024-01-18 12:35:36 +0530

    worked the exchange and refund policy page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { TermsConditionsComponent } from './policy/terms-conditions/terms-conditions.component';
 import { PrivacyPolicyComponent } from './policy/privacy-policy/privacy-policy.component';
+import { ExchangeRefundPolicyComponent } from './policy/exchange-refund-policy/exchange-refund-policy.component';
 
 export const routes: Routes = [
     {
@@ -23,6 +24,11 @@ export const routes: Routes = [
     {
         path:'privacy-policy',
         component:PrivacyPolicyComponent
+    },
+
+    {
+        path:'exchange-refund-policy',
+        component:ExchangeRefundPolicyComponent
     },
 ];
 
---
+++ src/app/policy/exchange-refund-policy/exchange-refund-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exchange-refund-policy',
+  standalone: true,
+  imports: [],
+  templateUrl: './exchange-refund-policy.component.html',
+  styleUrl: './exchange-refund-policy.component.css'
+})
+export class ExchangeRefundPolicyComponent {
+
+}
