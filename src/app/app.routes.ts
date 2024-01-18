078f203a42ae40b082472281d44dd5c6e66cb50f
Harish_9795 <jkaalees9597@gmail.com>
2024-01-18 12:22:24 +0530

    worked the terms and conditions page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { TermsConditionsComponent } from './policy/terms-conditions/terms-conditions.component';
+
 export const routes: Routes = [
     {
         path:'login',
@@ -10,6 +12,11 @@ export const routes: Routes = [
     {
         path:'register',
         component:RegisterComponent
+    },
+
+    {
+        path:'terms-conditions',
+        component:TermsConditionsComponent
     },
 ];
 
